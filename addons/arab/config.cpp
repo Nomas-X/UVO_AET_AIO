32c602c19289cc0fa2c77b27933f530e7e24d4d6
Nomas <nomasx2000@gmail.com>
2024-09-15 20:38:03 +0300

    arab PBO update and fixes
    - Removed uvo from uvo_arab folder name as the new prefix will be added automatically by hemtt.
    - Updated all classes to use `uvo_aet_aio` instead of `uvo` for the voices.

--- addons/arab/config.cpp
+++ addons/arab/config.cpp
@@ -1,10 +1,10 @@
 class CfgPatches {
-	class uvo_arab {
-		name = "uvo_arab";
+	class uvo_aet_aio_arab {
+		name = "uvo_aet_aio_arab";
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 1.9;
-		requiredAddons[] = {"uvo_main"};
+		requiredAddons[] = {"uvo_aet_aio_main"};
 		author = "Simplex Team";
 		url = "";
 		version = 1;
@@ -15,7 +15,7 @@ class CfgPatches {
 #include "voice_arab.hpp"
 
 class Extended_PreInit_EventHandlers {
-	class uvo_arab {
-		init = "[configFile >> 'UVO_voice_arab'] call UVO_fnc_register";
+	class uvo_aet_aio_arab {
+		init = "[configFile >> 'uvo_aet_aio_voice_arab'] call UVO_fnc_register";
 	};
 };
